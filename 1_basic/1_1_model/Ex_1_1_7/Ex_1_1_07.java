9fa2560bf7080f37f104b757c34559e2ab2ade74
XXSL980102 <3231408683@qq.com>
2020-07-09 10:35:06 +0800

    Chapter 1

---
+++ 1_basic/1_1_model/Ex_1_1_7/Ex_1_1_07.java
@@ -0,0 +1,25 @@
+
+public class Ex_1_1_07 {
+
+    public static void main(String[] args) {
+
+        double t = 9.0;
+		while(Math.abs(t-9.0/t) > .001)
+			t = (9.0/t + t) / 2.0;
+		StdOut.printf("%.5f", t);
+		
+		int sum=0;
+		for(int i = 1; i < 1000; i++)
+			for(int j = 0; j<i ; j++)
+				sum++;
+		StdOut.println(sum);
+		
+		int sum_ = 0;
+		for(int i=1; i< 1000; i *= 2)
+			for(int j=0; j< 1000; j++)
+				sum_++;
+		StdOut.println(sum_);
+
+    }
+
+}
